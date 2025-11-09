d13fb58f0cb4ba3d342a59426dee6025e9fb6aed
innerNULL <10429190+innerNULL@users.noreply.github.com>
2025-11-09 11:09:53 +0800

    leetgpu: Reverse array

---
+++ leetgpu/reverse-array/main-v20251109.1109.cu
@@ -0,0 +1,26 @@
+#include <cuda_runtime.h>
+
+__global__ void reverse_array(float* input, int N) {
+    const int block_idx = blockIdx.x;
+    const int block_dim = blockDim.x;
+    const int thread_idx = threadIdx.x;
+    const int index = block_idx * block_dim + thread_idx;
+    const int mid_point = (N - N % 2) / 2;
+    if (index < mid_point) {
+        const int symmetry_idx = N - 1 - index;
+        const float tmp = input[index];
+        input[index] = input[symmetry_idx];
+        input[symmetry_idx] = tmp;
+    }
+    return;
+}
+
+// input is device pointer
+extern "C" void solve(float* input, int N) {
+    const int threadsPerBlock = 256;
+    const int mid_point = (N - N % 2) / 2;
+    const int blocksPerGrid = (mid_point + threadsPerBlock - 1) / threadsPerBlock;
+
+    reverse_array<<<blocksPerGrid, threadsPerBlock>>>(input, N);
+    cudaDeviceSynchronize();
+}
